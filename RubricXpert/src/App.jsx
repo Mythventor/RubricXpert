09d037d483872535e66336aba9bfda7eb12adc79
Xing <151417971+Mythventor@users.noreply.github.com>
2025-01-21 22:26:15 -0600

    Update

--- RubricXpert/src/App.jsx
+++ RubricXpert/src/App.jsx
@@ -9,14 +9,14 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
+        <a>
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
+        <a>
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>RubricXpert</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -26,7 +26,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Under Construction
       </p>
     </>
   )
